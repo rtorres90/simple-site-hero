8e49b502c3ed4dd34f62c1357d2ec34836510830
Roberto Torres <Roberto.Torres@onbase.com>
2017-07-24 11:37:57 -0700

    first extensions

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
+"use strict";
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-var vscode = require('vscode');
+let vscode = require('vscode');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,7 +15,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
@@ -21,6 +23,18 @@ function activate(context) {
     });
 
     context.subscriptions.push(disposable);
+
+    let fileLinkDisposable = vscode.commands.registerCommand('extension.insertLink', () => {
+        vscode.window.showInformationMessage("fileLinkDisposable")
+    });
+
+    context.subscriptions.push(fileLinkDisposable);
+
+    let figureDisposable = vscode.commands.registerCommand('extension.insertFigure', () => {
+        vscode.window.showInformationMessage("figureDisposable")
+    });
+
+    context.subscriptions.push(figureDisposable);
 }
 exports.activate = activate;
 
