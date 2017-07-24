b92e7dd9f690edfae5881e4f57a5f36ca562bb21
Roberto Torres <Roberto.Torres@onbase.com>
2017-07-24 12:33:59 -0700

    config variables.

--- extension.js
+++ extension.js
@@ -20,6 +20,25 @@ let insertText = (value) => {
     });
 };
 
+let getImageTemplate = () =>{
+    return vscode.workspace.getConfiguration("staticSiteHero")["imagePathTemplate"];
+};
+
+let getFileTemplate = () =>{
+    return vscode.workspace.getConfiguration("staticSiteHero")["filePathTemplate"];
+};
+
+let updateTemplateWithDate = (template) => {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = ('0' + (today.getMonth() + 1)).slice(-2);
+
+    template = template.replace("${year}", year);
+    template = template.replace("${month}", month);
+
+    return template;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -29,10 +48,14 @@ function activate(context) {
     console.log('Congratulations, your extension "static-site-hero" is now active!');
 
     let fileLinkDisposable = vscode.commands.registerCommand('extension.insertLink', () => {
-        let linkTypeList = ["File", "Link"];
+        let linkTypeList = ["File", "Image"];
 
         vscode.window.showQuickPick(linkTypeList, {placeHolder: 'Link Type'}).then(result => {
-            insertText(result);
+            if(result === 'File'){
+                insertText("[Link Text](" + updateTemplateWithDate(getFileTemplate()) + ")");
+            }else if(result === 'Image'){
+                insertText("![Alt Text](" + updateTemplateWithDate(getImageTemplate()) + ")");
+            }
         });
     });
 
